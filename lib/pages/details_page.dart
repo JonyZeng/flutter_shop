930e3b52fd0f7e808769f9b2ad94886ee496aa24
Jony <949463214@qq.com>
2019-06-29 11:17:35 +0800

    local

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/provide/details_info.dart';
 import 'package:provide/provide.dart';
 
